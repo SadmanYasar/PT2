c0ef5d7226d85b8dfaaee36f2c2b2390ad7de619
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-02-02 01:52:07 +0800

    remove sample_project

--- sample-project/sample_progrm/20_header_files/circle.cpp
+++
@@ -1,8 +0,0 @@
-#include "include/system.hpp"
-
-#include "include/object.hpp"
-#include "include/circle.hpp"
-
-Circle::Circle(int _x, int _y, int r, int _color, int _speed) : Object(_x, _y, r, _color, _speed) {}
-
-void Circle::_drawShape() const { fillellipse(x, y, size, size); }
--- sample-project/sample_progrm/20_header_files/square.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/system.hpp"
-
-#include "include/object.hpp"
-#include "include/square.hpp"
-
-Square::Square(int _x, int _y, int w, int _color, int _speed) : Object(_x, _y, w, _color, _speed) {}
-void Square::_drawShape() const { bar(x, y, x + size, y + size); }
